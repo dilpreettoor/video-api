28f88fafc1c496f448357fdf7118c135bc6668b0
dilpreettoor <dilpreet.s.t@gmail.com>
2023-07-12 22:45:29 -0400

    added /videos and /videos/:id end points

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+// /videos endpoint
+app.get('/videos', (req, res) => {
+  fs.readFile('./server/src/data/videodata.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error reading videos data');
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// /videos/:id endpoint
+app.get('/videos/:id', (req, res) => {
+  fs.readFile('./server/src/data/videodata.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error getting video with the provided ID');
+    }
+
+    const videos = JSON.parse(data);
+    const video = videos.find((v) => v.id === req.params.id);
+
+    if (!video) {
+      return res.status(404).send('Video not found');
+    }
+
+    res.json(video);
+  });
+});
+
+// Start the server
+const port = 5050; 
+app.listen(port, () => {
+  console.log(`Welcome to Brainflix API, currently running on port:${port}`);
+});
